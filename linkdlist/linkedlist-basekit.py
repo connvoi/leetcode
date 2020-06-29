3bfd95a5f4ebfb47e57646bb5ea30085b4fc5197
Katsumi Yagi <connvoykagi@gmail.com>
2020-06-29 17:31:08 +0900

    updated

--- linkdlist/linkedlist-basekit.py
+++ linkdlist/linkedlist-basekit.py
@@ -35,7 +35,12 @@ class MyLinkedList:
         print('end- print_all-------')
     
         
+#初期の問題はlinkedlistの開始点が0で最終店がNoneだったんだけど、その前提がなくなっているので、開始店からnodeの1番目の数字が入っていないとだめみたい。
+#んなばかな。
 class Solution:
+    '''
+        https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1206/
+    '''
     def reverseList(self, node: ListNode) -> ListNode:
         head = None
         
@@ -50,21 +55,54 @@ class Solution:
             node = node.next
         
         return head
+    
+    '''
+        https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
+    '''
+    def removeElements(self, node: ListNode, val:int) -> ListNode:
+        print('--- debug ---')
+        head = node
+        pred = None
+        
+        while node != None:
+            print(node.val)
+            if node.val == val:
+                if pred != None:
+                    print('pred')
+                    print(node.next)
+                    pred.next = node.next
+                else:
+                    '''
+                    node.val == val and pred is Noneのときはいま見ているnodeがheadになる->predもNoneにしないといけない
+                    '''
+                    print('pred is none')
+                    head = node.next
+                    pred = node
+            else:
+                pred = node
+
+            node = node.next
+        
+        print('--- debug end ---')
+        return head
 
 
 
 
-linkedlist = ListNode(0)
+linkedlist = ListNode(1)
 listmaker = MyLinkedList()
 linkedlist=listmaker.addAtTail(linkedlist,1)
-linkedlist=listmaker.addAtTail(linkedlist,2)
-linkedlist=listmaker.addAtTail(linkedlist,3)
-linkedlist=listmaker.addAtTail(linkedlist,4)
-linkedlist=listmaker.addAtTail(linkedlist,5)
+#linkedlist=listmaker.addAtTail(linkedlist,2)
+#linkedlist=listmaker.addAtTail(linkedlist,6)
+#linkedlist=listmaker.addAtTail(linkedlist,3)
+#linkedlist=listmaker.addAtTail(linkedlist,4)
+#linkedlist=listmaker.addAtTail(linkedlist,5)
+#linkedlist=listmaker.addAtTail(linkedlist,6)
 #listmaker.print_all(linkedlist)
 #print(linkedList.head.next.val)
 solution = Solution()
-res = solution.reverseList(linkedlist)
+#res = solution.reverseList(linkedlist)
+res = solution.removeElements(linkedlist,1)
 listmaker.print_all(res)
 
 
