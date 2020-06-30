7f0a971d2634ff86e3f793958cd720c8f539f8c3
Katsumi Yagi <connvoykagi@gmail.com>
2020-06-30 16:28:08 +0900

    updated

---
+++ linkdlist/oddevenlinkedlist.py
@@ -0,0 +1,117 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList: 
+
+    def get(self, node: ListNode, index: int) -> int:
+        for _ in range(index+1):
+            node = node.next
+        
+        return node.val
+
+
+    def addAtTail(self, node: ListNode, val: int) -> ListNode:
+        head = node
+        while node.next != None:
+            node = node.next
+        
+        to_add = ListNode(val)
+        node.next = to_add
+
+        return head
+        
+
+    def print_all(self, node:ListNode) -> None:
+        print('start-print_all-------')
+        length =0
+        while node != None:
+            print(node.val)
+            node = node.next
+            length+=1
+        
+        print('len:'+ str(length))
+        print('end- print_all-------')
+    
+        
+#初期の問題はlinkedlistの開始点が0で最終店がNoneだったんだけど、その前提がなくなっているので、開始店からnodeの1番目の数字が入っていないとだめみたい。
+#んなばかな。
+
+'''
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+node numberをグループ化するので、node.valをグループ化したダメです!!!!
+'''
+class Solution:
+    '''
+        https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1208/
+    '''
+    def oddEvenList(self, node: ListNode) -> ListNode:
+        print('--- debug ---')
+        head = node
+        evenlist = None
+        oddlist = None
+        oddevenhead = None
+        count = 1
+        firstflag = 1
+
+        while node != None:
+            if node.val % 2 == 0:
+                #even
+                if evenlist == None:
+                    evenlist = ListNode(node.val)
+                    evenhead = evenlist
+                    if oddevenhead == None:
+                        oddevenhead = evenlist
+                        count+=1
+                else: 
+                    to_add = ListNode(node.val)
+                    evenlist.next = to_add
+                    evenlist = evenlist.next
+            else:
+                #odd
+                if oddlist == None:
+                    print(node.val)
+                    oddlist = ListNode(node.val)
+                    oddhead = oddlist
+                    if oddevenhead == None:
+                        oddevenhead = oddlist
+                        if count ==1: firstflag = 2
+                else: 
+                    to_add = ListNode(node.val)
+                    oddlist.next = to_add 
+                    oddlist = oddlist.next
+
+            node = node.next
+        
+        if firstflag ==1:
+            evenlist = oddhead
+        else:
+            print(firstflag)
+            oddlist = evenhead
+        
+        
+        print('--- debug end ---')
+        return evenhead
+
+
+
+
+linkedlist = ListNode(1)
+listmaker = MyLinkedList()
+linkedlist=listmaker.addAtTail(linkedlist,2)
+linkedlist=listmaker.addAtTail(linkedlist,3)
+linkedlist=listmaker.addAtTail(linkedlist,4)
+linkedlist=listmaker.addAtTail(linkedlist,5)
+#linkedlist=listmaker.addAtTail(linkedlist,6)
+#listmaker.print_all(linkedlist)
+#print(linkedList.head.next.val)
+solution = Solution()
+res = solution.oddEvenList(linkedlist)
+#res = solution.removeElements(linkedlist,1)
+listmaker.print_all(res)
+
+
