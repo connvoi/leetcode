7888ec446fbe2206ab783222a3939f5e20c1695a
Katsumi Yagi <connvoykagi@gmail.com>
2020-07-10 12:00:22 +0900

    updated

--- linkdlist/cyclicsortedlist.py
+++ linkdlist/cyclicsortedlist.py
@@ -50,9 +50,11 @@ class MyLinkedList:
             print(node.val)
             node = node.next
             length+=1
+            if node == None:
+                break
             if node.val == headval:
                 print('end cyclic list')
-                break;
+                break
         
         print('len:'+ str(length))
         print('end- print_all-------')
@@ -67,27 +69,44 @@ case2: listの最大値で最後に入れる場合 1~9のlistでinsertが10
 case3: 0を入れる場合(無条件でhead)
 case4: list自体がない場合は追加
 """
-
 class Solution:
-    def insert(self, head: ListNode, insertVal: int):
-        if head == None:
-            node = ListNode(insertVal)
-            node.next = None
-            return node
+    def insert(self, head: ListNode, insertVal: int) -> 'Node':
+        if not head:
+            head = ListNode(insertVal)
+            head.next = head
+            return head
+        
+        curr = head
 
-        node = head
-
-
-        while node != None:
-            if node.val < insertVal and insertVal < node.next.val:
-                to_add = ListNode(insertVal) 
-                to_add.next = node.next
-                node.next = to_add
-                node = node.next
+        while True:
+            #currentより大きくnextより小さい->listに挿入する
+            if curr.val <= insertVal <= curr.next.val:
                 break
-            node = node.next
-            if node.val == head.val:
+            
+            # last element
+            if curr.val>curr.next.val:
+                #currentより大きく、nextよりも大きい->最後のエレメント
+                if curr.val <= insertVal >= curr.next.val:
+                    break
+                #currentとnextより小さい->最初のエレメント
+                #cyclicなので最初と最後は一緒
+                elif curr.val >= insertVal <= curr.next.val:
+                    break
+                    
+            curr = curr.next 
+            # all elements are equal
+            if curr == head:
                 break
+
+        #new_nodeで作成
+        new_node = ListNode(insertVal)
+        #tmpに入れる
+        tmp = curr.next
+        #currentのnextにnewを入れる
+        curr.next = new_node
+        #new_nodeのnextにtmpを入れる
+        new_node.next = tmp
+        #linkedlistは入れたときにtmpなりに確保しておくとheadを移せる。忘れがちなので忘れないようにする。
         return head
 
 
@@ -100,7 +119,7 @@ linkedlist=listmaker.connectTailtoHead(linkedlist)
 
 
 solution = Solution()
-res = solution.insert(linkedlist, 2)
+res = solution.insert(linkedlist, 0)
 listmaker.print_cyclic(res)
 #print(res)
 
